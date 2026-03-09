f50b90594c0ce83a774fd40e0244f7b128c9777e
Стоян Андреев Томов <SATomov23@codingburgas.bg>
2026-03-09 16:02:18 +0200

    Add Movie Section

--- StoyanTomovPP/StoyanTomovPP/CinemaMenu.cpp
+++ StoyanTomovPP/StoyanTomovPP/CinemaMenu.cpp
@@ -2,11 +2,47 @@
 #include <fstream>
 using namespace std;
 
-void print()
+void printMenu()
 {
-	cout << ifstream("CinemaMenu.txt").rdbuf();
+    ifstream file("CinemaMenu.txt");
+    if (!file)
+    {
+        cout << "Error: Could not open CinemaMenu.txt\n";
+        return;
+    }
+    cout << file.rdbuf();
 }
+
+void moviesSection()
+{
+    cout << "\n===== MOVIES SECTION =====\n";
+    cout << "This is where movies will be shown.\n\n";
+}
+
 int main()
 {
-	print();
+    int choice;
+
+    while (true)
+    {
+        printMenu();
+
+        cout << "\nEnter choice: ";
+        cin >> choice;
+
+        if (choice == 1)
+        {
+            moviesSection();
+        }
+        else
+        {
+            cout << "Option not implemented yet.\n";
+        }
+
+        cout << "\nPress Enter to continue...";
+        cin.ignore();
+        cin.get();
+    }
+
+    return 0;
 }
---
+++ StoyanTomovPP/StoyanTomovPP/CinemaMenu.h
@@ -0,0 +1,4 @@
+#pragma once
+
+void showMainMenu();
+void moviesSection();
